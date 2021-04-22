f5bac417401e014737cf4d2752d742cf85bf38b0
yaztaha <yazi.taha@gmail.com>
2021-04-22 22:21:58 +0000

    poly_integrate

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,18 +4,16 @@
 
 def poly_integral(poly, C=0):
     """method of new coeffiecients for an integral"""
-    if not isinstance(poly, list) or len(poly) == 0:
+    if poly == [] or type(poly) is not list or type(C) is not int:
         return None
-    if not isinstance(C, int):
-        return None
-    if poly == [0]:
+    if len(poly) == 1:
         return [C]
+    result = [C]
 
-    lis = [C]
     for i in range(len(poly)):
-        if (poly[i] % (i + 1)) == 0:
-            new = int(poly[i]/(i + 1))
-        else:
-            new = poly[i]/(i + 1)
-            lis.append(new)
-    return lis
+        integ = poly[i] / (i+1)
+        if inte.is_integer():
+            integ = int(r)
+            result.append(integ)
+
+        return result
