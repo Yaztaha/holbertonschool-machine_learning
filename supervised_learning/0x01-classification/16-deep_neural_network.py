ef712e35071f4a4dcc38ad5aa27b03eb0f12e9e7
yaztaha <yazi.taha@gmail.com>
2021-05-10 09:46:21 +0000

    DNN_init_he_et_al

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -6,12 +6,12 @@ import numpy as np
 class DeepNeuralNetwork():
     """ DNN class """
     def __init__(self, nx, layers):
-        """ constructor method """
-        if type(nx) != int:
+        """init the class"""
+        if type(nx) is not int:
             raise TypeError('nx must be an integer')
-        if nx < 0:
+        if nx < 1:
             raise ValueError('nx must be a positive integer')
-        if type(layers) != list or len(layers) == 0:
+        if type(layers) is not list or not layers:
             raise TypeError('layers must be a list of positive integers')
 
         self.L = len(layers)
@@ -19,15 +19,15 @@ class DeepNeuralNetwork():
         self.weights = {}
 
         for i in range(self.L):
-            if type(layers[i]) != int or layers[i] <= 0:
+            if type(layers[i]) is not int or layers[i] <= 0:
                 raise TypeError('layers must be a list of positive integers')
 
             wi = 'W'+str(i + 1)
             bi = 'b'+str(i + 1)
             if i == 0:
                 self.weights[wi] = np.random.randn(layers[i], nx)\
-                                  * np.sqrt(2./nx)
+                                   * np.sqrt(2./nx)
             else:
-                self.weights[wi] = np.random.randn(layers[i], layers[i - 1])\
-                                   * np.sqrt(2/layers[i - 1])
+                self.weights[wi] = np.random.randn(layers[i], layers[i-1])\
+                                   * np.sqrt(2/layers[i-1])
             self.weights[bi] = np.zeros((layers[i], 1))
