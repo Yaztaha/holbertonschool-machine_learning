91c71d93a41b199bf54649a381ff12d49858ca32
yaztaha <yazi.taha@gmail.com>
2021-05-13 15:42:25 +0000

    [TF_forward_prop]

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -5,7 +5,8 @@ create_layer = __import__('1-create_layer').create_layer
 
 def forward_prop(x, layer_size=[], activations=[]):
     """ forward propagation function """
-    ph = create_layer(x, layer_size[0], activations[0])
+    lyr = create_layer(x, layer_size[0], activations[0])
     for i in range(1, len(layer_size)):
-        layer = create_layer(ph, layer_size[i], activations[i])
-    return layer
+        lyr = create_layer(lyr, layer_size[i], activations[i])
+
+    return lyr
