d5d73e21554532beeff3dd04d08c87cf9e4e32d8
yaztaha <yazi.taha@gmail.com>
2021-05-20 13:38:52 +0000

    RMSProp_algorithm

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -6,5 +6,5 @@ import numpy as np
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """ Function RMSProp """
     up_s = (beta2 * s) + ((1 - beta2) * grad * grad)
-    up_var = var - ((alpha * grad) / (((new_s)**0.5) + epsilon))
+    up_var = var - ((alpha * grad) / (((up_s)**0.5) + epsilon))
     return (up_var, up_s)
