c18ce77d580e0c65b2545984c49d3db5af5da8a4
yaztaha <yazi.taha@gmail.com>
2021-05-24 14:15:45 +0000

    f1_score_cm

---
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" F1_score of confusion matrix """
+
+
+import numpy as np
+sntv = __import__('1-sensitivity').sensitivity
+prcs = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """ function that return f1 score """
+    _sntv = sensitivity(confusion)
+    _prcs = precision(confusion)
+    F1_score = 2 * ((_precision * _sensitivity) / (_precision + _sensitivity))
+
+    return F1_score
