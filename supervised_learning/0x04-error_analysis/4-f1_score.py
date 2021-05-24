f328b5910f0ab8ec988e56ca4fb3a17c41e310f6
yaztaha <yazi.taha@gmail.com>
2021-05-24 14:20:59 +0000

    f1_score_cm

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -3,14 +3,13 @@
 
 
 import numpy as np
-sntv = __import__('1-sensitivity').sensitivity
-prcs = __import__('2-precision').precision
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
     """ function that return f1 score """
-    _sntv = sensitivity(confusion)
-    _prcs = precision(confusion)
-    F1_score = 2 * ((_prcs * _sntv) / (_prcs + _sntv))
-
-    return F1_score
+    PPV = precision(confusion)
+    TPR = sensitivity(confusion)
+    F1 = 2 * PPV * TPR / (PPV + TPR)
+    return F1
