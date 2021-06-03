400088cd322def4b1e7bc0a56ede709b59e704c0
yaztaha <yazi.taha@gmail.com>
2021-06-03 00:33:20 +0000

    grayscale_conv

---
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Convolution on grayscale img """
+
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """ method that convolve on grayscale img """
+     m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    n_h = h - kh + 1
+    n_w = w - kw + 1
+    convolved = np.zeros([m, n_h, n_w])
+    for x in range(n_h):
+        for y in range(n_w):
+            image = images[:, x:x+kh, y:y+kw]
+            convolved[:, x, y] = np.multiply(image, kernel).sum(axis=(1, 2))
+    return convolved
