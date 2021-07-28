ffa3b810ac1fcb1afb815e39b59788a2d7ba32f9
yaztaha <yazi.taha@gmail.com>
2021-07-28 21:30:32 +0000

    symmetric_pAffinities

---
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Symmetric P affinities """
+import numpy as np
+P_init = __import__('2-P_init').P_init
+HP = __import__('3-entropy').HP
+
+
+def P_affinities(X, tol=1e-5, perplexity=30.0):
+    """ Symmetric P affinities function """
+    n, _ = X.shape
+    D, P, betas, H = P_init(X, perplexity)
+    for i in range(n):
+        high = None
+        low = None
+        Di = np.delete(D[i], i, axis=0)
+        Hi, Pi = HP(Di, betas[i])
+        diff = Hi - H
+        # binary search
+        while np.abs(diff) > tol:
+            if diff > 0:
+                low = betas[i, 0]
+                if high is None:
+                    betas[i, 0] = betas[i, 0] * 2
+                else:
+                    betas[i, 0] = (betas[i, 0] + high) / 2
+            else:
+                high = betas[i, 0]
+                if low is None:
+                    betas[i, 0] = betas[i, 0] / 2
+                else:
+                    betas[i, 0] = (betas[i, 0] + low) / 2
+            Hi, Pi = HP(Di, betas[i])
+            diff = Hi - H
+        Pi = np.insert(Pi, i, 0)
+        P[i] = Pi
+    P = (P + P.T)/(2*n)
+    return(P)
