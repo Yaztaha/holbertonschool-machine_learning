0c87ec6cae288a2fdf0023df8a5b6e87ad8180f9
yaztaha <yazi.taha@gmail.com>
2021-07-28 21:21:49 +0000

    var_init

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -6,9 +6,12 @@ import numpy as np
 def P_init(X, perplexity):
     """ Var init function """
     n, d = X.shape
+
     sum_X = np.sum(np.square(X), 1)
-    D = np.add(np.add(-2 * np.matmul(X, X.T), sum_X).T, sum_X)
-    P = np.zeros((n, n))
-    betas = np.ones((n, 1))
+    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    np.fill_diagonal(D, 0)
+
+    P = np.zeros([n, n])
+    betas = np.ones([n, 1])
     H = np.log2(perplexity)
-    return D, P, betas, H
+    return (D, P, betas, H)
