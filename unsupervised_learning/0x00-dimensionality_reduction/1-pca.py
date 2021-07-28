51ab1bb4a7dde9be81d8c889762a2b3518a154e5
yaztaha <yazi.taha@gmail.com>
2021-07-28 21:16:47 +0000

    PCA_2

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -4,10 +4,9 @@ import numpy as np
 
 
 def pca(X, ndim):
-    """Function PCA2 with dim"""
-    m, n = X.shape
-    X = np.mean(X, axis=0) - X
-    U, Sigma, Vh = np.linalg.svd(X, full_matrices=True)
-    new_Vh = Vh.T[:, :ndim]
-    T = np.matmul(X, new_Vh)
-    return (T)
+    """ Function PCA2 with dim """
+    X -= np.mean(X, axis=0)
+    u, s, vh = np.linalg.svd(X)
+    w = vh.T
+    T = np.matmul(X, w[:, :ndim])
+    return T
