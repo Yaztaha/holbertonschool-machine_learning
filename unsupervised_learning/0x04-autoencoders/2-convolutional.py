041f36a1d655493dc23997bb4f4ce81fb5fb05bd
yaztaha <yazi.taha@gmail.com>
2021-08-26 18:15:22 +0000

    conv_autoencoder

---
+++ unsupervised_learning/0x04-autoencoders/2-convolutional.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+""" Conv autoencoder """
+import tensorflow.keras as keras
+K = keras
+
+
+def autoencoder(input_dims, filters, latent_dims):
+    """ Conv autoencoder function """
+
+    encoder_inputs = K.Input(shape=input_dims)
+    for i in range(len(filters)):
+        layer = K.layers.Conv2D(filters=filters[i],
+                                kernel_size=(3, 3),
+                                strides=(1, 1),
+                                padding='same',
+                                activation='relu')
+        if i == 0:
+            outputs = layer(encoder_inputs)
+        else:
+            outputs = layer(outputs)
+        layer = K.layers.MaxPool2D(pool_size=(2, 2),
+                                   strides=None,
+                                   padding='same')
+        outputs = layer(outputs)
+    encoder = K.models.Model(inputs=encoder_inputs, outputs=outputs)
+
+    # Define the decoder model
+    decoder_inputs = K.Input(shape=latent_dims)
+    for i in range(len(filters) - 1, -1, -1):
+        if i != 0:
+            layer = K.layers.Conv2D(filters=filters[i],
+                                    kernel_size=(3, 3),
+                                    strides=(1, 1),
+                                    padding='same',
+                                    activation='relu')
+            if i == len(filters) - 1:
+                outputs = layer(decoder_inputs)
+            else:
+                outputs = layer(outputs)
+        else:
+            layer = K.layers.Conv2D(filters=filters[i],
+                                    kernel_size=(3, 3),
+                                    strides=(1, 1),
+                                    padding='valid',
+                                    activation='relu')
+            outputs = layer(outputs)
+        layer = K.layers.UpSampling2D(size=(2, 2))
+        outputs = layer(outputs)
+    layer = K.layers.Conv2D(filters=input_dims[-1],
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            padding='same',
+                            activation='sigmoid')
+    outputs = layer(outputs)
+    decoder = K.models.Model(inputs=decoder_inputs, outputs=outputs)
+
+    outputs = encoder(encoder_inputs)
+    outputs = decoder(outputs)
+    auto = K.models.Model(inputs=encoder_inputs, outputs=outputs)
+
+    auto.compile(optimizer='Adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
