447a6f18036b9d9a53bf4b3beb57df1a3988ba84
yaztaha <yazi.taha@gmail.com>
2021-09-02 17:59:46 +0000

    gru_cell

--- supervised_learning/0x0D-RNNs/2-gru_cell.py
+++ supervised_learning/0x0D-RNNs/2-gru_cell.py
@@ -1,41 +1,33 @@
 #!/usr/bin/env python3
-""" GRU cell module """
+""" GRU module """
 import numpy as np
 
 
 class GRUCell:
-    """ GRU cell class """
+    """ GRU class """
     def __init__(self, i, h, o):
         """ class constructor """
-        self.Wz = np.random.randn(h + i, h)
-        self.Wr = np.random.randn(h + i, h)
-        self.Wh = np.random.randn(h + i, h)
-        self.Wy = np.random.randn(h, o)
+        self.Wz = np.random.normal(size=(i+h, h))
+        self.Wr = np.random.normal(size=(i+h, h))
+        self.Wh = np.random.normal(size=(i+h, h))
+        self.Wy = np.random.normal(size=(h, o))
         self.bz = np.zeros((1, h))
         self.br = np.zeros((1, h))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
-        """ forward prop method """
-        m, i = x_t.shape
-        _, h = h_prev.shape
-        st_ct_1 = np.hstack((h_prev, x_t))
-        g_u = self.sigmoid(np.matmul(st_ct_1, self.Wz) + self.bz)
-        g_r = self.sigmoid(np.matmul(st_ct_1, self.Wr) + self.br)
-        st_ct_full = np.hstack(((g_r * h_prev), x_t))
-        c_tilde = np.tanh(np.matmul(st_ct_full, self.Wh) + self.bh)
-        h_next = (g_u * c_tilde) + ((1-g_u) * h_prev)
-        y_n = np.matmul(h_next, self.Wy) + self.by
-        y = self.softmax(y_n)
-        return (h_next, y)
+        """ forwad prop methjod """
+        new_w = np.concatenate((h_prev, x_t), axis=1)
+        z = np.matmul(new_w, self.Wz) + self.bz
+        z = 1 / (1 + np.exp(-z))
+        r = np.matmul(new_w, self.Wr) + self.br
+        r = 1 / (1 + np.exp(-r))
 
-    def softmax(self, X):
-        """ softmax method """
-        np.exp(X)
-        expo_sum = np.sum(np.exp(X), axis=-1, keepdims=True)
-        return expo/expo_sum
+        new_w2 = np.concatenate((r * h_prev, x_t), axis=1)
+        h = np.tanh(np.matmul(new_w2, self.Wh) + self.bh)
+        h_next = (1 - z) * h_prev + z * h
 
-    def sigmoid(self, X):
-        """ sigmoid method """
-        return 1/(1 + np.exp(-X))
+        y_mul = np.matmul(h_next, self.Wy) + self.by
+        y = np.exp(y_mul) / np.sum(np.exp(y_mul), axis=1, keepdims=True)
+        return h_next, y
