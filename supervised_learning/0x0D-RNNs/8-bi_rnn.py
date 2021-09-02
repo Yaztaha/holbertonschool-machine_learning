7dde68e92b0d5598da9e32b97a5e802011dfba0b
yaztaha <yazi.taha@gmail.com>
2021-09-02 18:07:30 +0000

    bi_rnn

--- supervised_learning/0x0D-RNNs/8-bi_rnn.py
+++ supervised_learning/0x0D-RNNs/8-bi_rnn.py
@@ -6,14 +6,18 @@ import numpy as np
 def bi_rnn(bi_cell, X, h_0, h_t):
     """ bi_rnn forward prop """
     t, m, i = X.shape
-    _, h = h_0.shape
-    H = np.zeros((t, m, 2*h))
-    H[0, :, 0:h] = h_0
-    H[t-1, :, h:2*h] = h_t
-    for iter in range(t):
-        H[iter, :, 0:h] = bi_cell.forward(H[iter-1, :, 0:h], X[iter])
-        H[t-iter-2, :, h:2*h] = bi_cell.backward(H[t-1-iter, :, h:2*h],
-                                                 X[t-1-iter])
-    H[t-1, :, h:2*h] = 0
+    h = h_0.shape[1]
+    h_for = np.zeros((t, m, h))
+    h_back = np.zeros((t, m, h))
+    h_ft = h_0
+    h_bt = h_t
+    for i in range(t):
+        x_ft = X[i]
+        x_bt = X[-(i+1)]
+        h_ft = bi_cell.forward(h_ft, x_ft)
+        h_bt = bi_cell.backward(h_bt, x_bt)
+        h_for[i] = h_ft
+        h_back[-(i+1)] = h_bt
+    H = np.concatenate((h_for, h_back), axis=-1)
     Y = bi_cell.output(H)
-    return(H, Y)
+    return H, Y
